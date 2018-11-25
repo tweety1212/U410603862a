cdefbff5376fe13674cf5e770a17ff7e72b350a1
tweety1212 <tweety880402@gmail.com>
2018-11-26 01:41:39 +0800

    Q10

---
+++ Assets/MyCharacter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCharacter : MonoBehaviour {
+	public bool movable = false;
+	CharacterController controller;
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+
+	void Update () {
+		Vector3 speed = new Vector3(0, 0, 0);
+		if (movable)
+		{
+			speed.x = -5;
+		}
+		controller.SimpleMove(speed);
+	}
+
+	public void CharacterWalk() {
+		movable = true;
+		//GetComponent<Animation>().Play("walk");
+	}
+
+	public void CharacterStop() {
+		movable = false;
+		//GetComponent<Animation>().Play("idle");
+	}
+}
---
+++ Assets/SwitchCollisionEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchCollisionEvent : MonoBehaviour {
+	public MyCharacter character;
+	bool SwitchEnable = true;
+
+	void OnCollisionEnter(Collision collision) {
+		if (SwitchEnable == true )
+		{
+			character.CharacterWalk();
+	        SwitchEnable = false;
+		}
+	}
+}
